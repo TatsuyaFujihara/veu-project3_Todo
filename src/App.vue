78f1f9423be7b1bb72c6dfb394a5128b476c49f7
藤原　龍也 <fujiharatatsuya@fujiwaratatsuyanoMacBook-Pro.local>
2021-09-11 16:13:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <router-link to="Todo">todoへ</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    toTodo(){
+      this.$router.push('/todo')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
